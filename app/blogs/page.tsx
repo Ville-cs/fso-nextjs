a5bac1f1cf02e6c6cbe3771b2496f7d3b7d23d65
Ville <ville-poikela@hotmail.com>
2026-07-14 19:07:22 +0300

    adds filtering of blogs using  url params

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,11 +1,27 @@
 import { getBlogs } from "../services/blogs";
 import Link from "next/link";
 
-const Blogs = () => {
-  const blogs = getBlogs().sort((a, b) => b.likes - a.likes);
+const Blogs = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ filter?: string }>;
+}) => {
+  const { filter } = await searchParams;
+  const allBlogs = getBlogs();
+  const blogs = filter
+    ? allBlogs.filter((blog) =>
+        blog.title.toLowerCase().includes(filter.toLowerCase()),
+      )
+    : allBlogs;
+  blogs.sort((a, b) => b.likes - a.likes);
+
   return (
     <div>
       <h2>Blogs</h2>
+      <form action="/blogs">
+        <input type="text" name="filter" defaultValue={filter || ""} />
+        <button type="submit">filter blogs by title</button>
+      </form>
       <ul>
         {blogs.map((blog) => (
           <li key={blog.id}>
