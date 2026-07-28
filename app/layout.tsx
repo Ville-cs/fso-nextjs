6e908bced28018bb9f36917cb9f47217c9b3660e
Ville <ville-poikela@hotmail.com>
2026-07-28 13:07:12 +0300

    adds shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,11 @@ import AuthSessionProvider from "./components/SessionProvider";
 import NavBar from "./components/NavBar";
 import { NotificationProvider } from "./components/NotificationContext";
 import Notification from "./components/Notification";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
 
 export default function RootLayout({
   children,
@@ -10,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", geist.variable)}>
       <body className="min-h-screen">
         <AuthSessionProvider>
           <NotificationProvider>
