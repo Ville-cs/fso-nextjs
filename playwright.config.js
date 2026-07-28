4ff48401d28aaa91ecc497e39de42805b64d5e5a
Ville <ville-poikela@hotmail.com>
2026-07-28 11:32:04 +0300

    Lowers max times for tests

--- playwright.config.js
+++ playwright.config.js
@@ -5,11 +5,11 @@ export default defineConfig({
   testDir: "tests",
 
   // Timeout for an individual test
-  timeout: 60_000,
+  timeout: 15_000,
 
   // Timeout for an expect action
   expect: {
-    timeout: 45_000,
+    timeout: 10_000,
   },
 
   // Run all tests in parallel.
